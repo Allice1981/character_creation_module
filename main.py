8354be834c6af832ecec65014d1bba761ccb5de7
Муравцева Олеся <allice1981@yandex.ru>
2023-02-12 15:57:39 +0600

    Add first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(Василис, char_class):
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
     if char_class == 'mage':
